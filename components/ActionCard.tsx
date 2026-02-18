b590e6f8f8f631021b217dec82f9eb9162fe5197
Vibecode Terminal <terminal@vibecodeapp.com>
2026-02-18 22:33:35 +0000

    Sandbox autosave

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -364,7 +364,7 @@ export default function ActionCard({
             <svg className="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zM12.75 12a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
             </svg>
-            <span>Challenge a Friend</span>
+            <span>Challenge a friend</span>
           </button>
 
           {/* Comments Button */}
