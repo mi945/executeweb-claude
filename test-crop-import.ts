43637f2e6e85603316cf21303ef85b74309819f8
Vibecode Terminal <terminal@vibecodeapp.com>
2026-03-10 06:20:05 +0000

    Sandbox autosave

---
+++ test-crop-import.ts
@@ -0,0 +1,5 @@
+// Test file to verify react-easy-crop can be imported
+import Cropper from 'react-easy-crop';
+import type { Area, Point } from 'react-easy-crop';
+
+console.log('Imports successful');
